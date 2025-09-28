cd7157367b65db589c644615d91b84eda809706d
Siddhant Mishra <151660891+heyysiddhant@users.noreply.github.com>
2025-09-28 22:07:53 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { Typewriter } from 'react-simple-typewriter';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
-import avtarImage from '../assets/siddy.jpg';
+import avtarImage from '../assets/siddy2.png';
 
 const techStack = [
   'React.js', 'Node.js', 'MongoDB', 'Express.js', 'Tailwind CSS', 'Figma',
